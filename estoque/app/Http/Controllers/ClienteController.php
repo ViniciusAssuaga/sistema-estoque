4b80f9dd25eac0836f3bc128b67f434ff9be5c74
VINÍCIUS ASSUAGA BOARO <viniciusboaro@pge.sc.gov.br>
2026-08-10 14:08:37 -0300

    adicionado title nos botões de editar/excluir cliente

--- estoque/app/Http/Controllers/ClienteController.php
+++ estoque/app/Http/Controllers/ClienteController.php
@@ -16,10 +16,10 @@ class ClienteController extends Controller
             return DataTables::of($data)
                 ->addColumn('acoes', function($row){
                     return '
-                        <button type="button" class="btn btn-sm btn-outline-laravel py-0 px-2 btn-editar" data-id="'.$row->id.'">
+                        <button type="button" class="btn btn-sm btn-outline-laravel py-0 px-2 btn-editar" data-id="'.$row->id.'" title="Editar Cliente">
                             <i class="bi bi-pencil-square"></i>
                         </button>
-                        <button type="button" class="btn btn-sm btn-outline-danger py-0 px-2 btn-excluir" data-id="'.$row->id.'">
+                        <button type="button" class="btn btn-sm btn-outline-danger py-0 px-2 btn-excluir" data-id="'.$row->id.'" title="Excluir Cliente">
                             <i class="bi bi-trash"></i>
                         </button>
                     ';
