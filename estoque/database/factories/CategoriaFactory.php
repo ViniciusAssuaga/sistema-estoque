2f997b547ab00524ec2b2dea1e9c829c5652f5c2
VINÍCIUS ASSUAGA BOARO <viniciusboaro@pge.sc.gov.br>
2026-08-14 14:23:30 -0300

    Corrige CategoriaFactory para garantir nomes unicos e evitar conflito

--- estoque/database/factories/CategoriaFactory.php
+++ estoque/database/factories/CategoriaFactory.php
@@ -3,17 +3,14 @@
 namespace Database\Factories;
 
 use Illuminate\Database\Eloquent\Factories\Factory;
-use Faker\Factory as FakerFactory; // <--- Importa o Faker oficial
 
 class CategoriaFactory extends Factory
 {
     public function definition(): array
     {
-        $faker = FakerFactory::create('pt_BR'); // <--- Cria a instância manualmente (opcionalmente em português!)
-
         return [
-            'nome'      => $faker->unique()->word(),
-            'descricao' => $faker->sentence(),
+            'nome'      => fake()->unique()->word() . uniqid(),
+            'descricao' => fake()->sentence(),
         ];
     }
 }
