aff7b2e7434944aee1f409f90173e80ff0542bac
VINÍCIUS ASSUAGA BOARO <viniciusboaro@pge.sc.gov.br>
2026-08-14 13:57:42 -0300

    Executa DatabaseSeeder via migration em produção

---
+++ estoque/database/migrations/2026_08_14_165707_run_database_seeder_once.php
@@ -0,0 +1,15 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Support\Facades\Artisan;
+
+return new class extends Migration {
+    public function up() {
+        // Executa o DatabaseSeeder completo com todas as factories
+        Artisan::call('db:seed', ['--force' => true]);
+    }
+
+    public function down() {
+        // Opcional
+    }
+};
