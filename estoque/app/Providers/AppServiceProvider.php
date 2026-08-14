47272264b47fe063b66d13343113a2c472ba63a7
VINÍCIUS ASSUAGA BOARO <viniciusboaro@pge.sc.gov.br>
2026-08-14 10:55:44 -0300

    fix: force https scheme in boot method

--- estoque/app/Providers/AppServiceProvider.php
+++ estoque/app/Providers/AppServiceProvider.php
@@ -4,6 +4,7 @@ namespace App\Providers;
 
 use Illuminate\Pagination\Paginator;
 use Illuminate\Support\ServiceProvider;
+use Illuminate\Support\Facades\URL;
 
 class AppServiceProvider extends ServiceProvider
 {
@@ -21,6 +22,11 @@ class AppServiceProvider extends ServiceProvider
     public function boot(): void
     {
         Paginator::useBootstrapFive();
+
+        // Força todos os links e assets a usarem HTTPS na nuvem
+        if (config('app.env') === 'production' || app()->environment('production')) {
+            URL::forceScheme('https');
+        }
     }
 }
 
