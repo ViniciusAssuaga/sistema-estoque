11207d6c47988ea26ce5b6eff145896bd2fafe9a
vi-pge <viniciusboaro@pge.sc.gov.br>
2026-08-21 13:10:49 -0300

    seeder removido

--- estoque/database/seeders/DatabaseSeeder.php
+++ estoque/database/seeders/DatabaseSeeder.php
@@ -14,18 +14,18 @@ class DatabaseSeeder extends Seeder
     public function run(): void
     {
         // Gera 15 registros de categorias de teste
-        Categoria::factory()->count(15)->create();
+        // Categoria::factory()->count(15)->create();
 
         // Gera 10.000 registros de produtos
-        Produto::factory()->count(10000)->create();
+        // Produto::factory()->count(10000)->create();
 
         // Gera 50 registros de clientes de teste
-        Cliente::factory()->count(50)->create();
+        // Cliente::factory()->count(50)->create();
 
         // Gera 50 registros de fornecedores de teste
-        Fornecedor::factory()->count(50)->create();
+        // Fornecedor::factory()->count(50)->create();
 
         // Gera 100 registros de movimentações de estoque
-        Movimentacao::factory()->count(100)->create();
+        // Movimentacao::factory()->count(100)->create();
     }
 }
