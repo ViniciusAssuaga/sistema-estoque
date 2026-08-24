d0003b6cce79ff9360ff73b8f0fc3891cabc998b
VINÍCIUS ASSUAGA BOARO <viniciusboaro@pge.sc.gov.br>
2026-08-24 15:05:11 -0300

    alteração teste

--- estoque/resources/views/layouts/app.blade.php
+++ estoque/resources/views/layouts/app.blade.php
@@ -16,7 +16,7 @@
 
     @stack('styles')
 
-    <!-- Asset Bundler Oficial do Laravel (Vite) -->
+    <!-- Asset Bundler Oficial do Laravel (Vite) alteração teste main -->
     @vite(['resources/css/app.css', 'resources/js/app.js'])
 
     <style>
